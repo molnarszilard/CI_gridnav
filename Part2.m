22997be153530d9e1dbf8c59ae0870a328d53630
molnarszilard <60471017+molnarszilard@users.noreply.github.com>
2021-03-24 21:59:27 +0200

    Update Part2.m

--- Part2.m
+++ Part2.m
@@ -1,16 +1,24 @@
-cfg.size = [5 5];
-max_x=cfg.size(1);
-max_y=cfg.size(2);
-cfg.x_goal = randi([1 5],1,2)';
-cfg.x_obst = [;];  
+%% Initial values
+epsQiter=100; 
+epshiter=0; 
+epsQeval=0.1; 
+discount = 0.95; % initial gamma
+
+%% creating the map
+cfg.size = [5 5]; % generate a 5-by-5 map
+% max_x=cfg.size(1);
+% max_y=cfg.size(2);
+cfg.x_goal = randi([1 5],1,2)'; %give a random goal
+% cfg.x_goal = [2;5]; % an example case
+% cfg.x_obst = [1 1,5,4,3;2,5,3,4,1];  % an example case
 nr_of_obstacles=5;
 n=0;
-while n<nr_of_obstacles
+while n<nr_of_obstacles % generate the obstacles
    new_obst = randi([1 5],1,2)';
    if cfg.x_goal~=new_obst
       exist=0;
          for i=1:n
-           if cfg.x_obst(:,i)==new_obst;
+           if cfg.x_obst(:,i)==new_obst
               exist=1;
            end
          end    
@@ -21,66 +29,41 @@ while n<nr_of_obstacles
     end
    
 end
-start_loc=[;];
-while isempty(start_loc)
-    new_start = randi([1 5],1,2)';
-    if cfg.x_goal~=new_start
-      exist=0;
-      for i=1:nr_of_obstacles
-         if cfg.x_obst(:,i)==new_start;
-            exist=1;
-         end
-      end    
-      if exist==0
-         start_loc=new_start;
-         n=n+1; %folosleges
-      end
-    end
-end
-for i=1:nr_of_obstacles
-   u=[1,1,1,1];
-   if cfg.x_obst(:,i)==start_loc+[-1;0] | start_loc(1)==1
-      u(1)=0;
-   end
-   if cfg.x_obst(:,i)==start_loc+[1;0] | start_loc(1)==5
-      u(2)=0;
-   end
-   if cfg.x_obst(:,i)==start_loc+[0-1] | start_loc(2)==1
-      u(3)=0;
-   end
-   if cfg.x_obst(:,i)==start_loc+[0;1] | start_loc(2)==5
-      u(4)=0;
-   end
-end
-if u(1)
-    ustart=1;
-elseif u(2)
-    ustart=2;
-elseif u(3)
-    ustart=3;
-elseif u(4)
-    ustart=4;
-else
-    'Please re-run the code, you are blocked!'  
-end
 model = gridnav_problem('model', cfg);
-[xplus, rplus, terminal] = gridnav_mdp(model, start_loc, ustart);  
+
+%% Partea 2 - Calculating the Q and h + Partea 3 time/iter_nr comparation
+%Iteratia Q
+tic
+[Q_optim1, h_optim1, iteratii] = iteratiaQ(discount,epsQiter,epsQeval, model);
+iteratii
+toc
+
+%Legea de control cu epsQeval mai mic
+tic
+[Q_optim2, h_optim2, iteratiiMajore, iteratiiMinore] = legeaDeControl(discount, epshiter, epsQeval, epsQiter, model);
+toc
+iteratiiMajore
+iteratiiMinore
+%Legea de control cu epsQeval mai mare
+epsQeval22=0.9;
+tic
+[Q_optim22, h_optim22, iteratiiMajore22, iteratiiMinore22] = legeaDeControl(discount, epshiter, epsQeval22, epsQiter, model);
+toc
+iteratiiMajore22
+iteratiiMinore22
+
+%% Move the robot according to the calculated control
 viscfg = struct;
 viscfg.model = model;
-viscfg.x = xplus;
-epsQiter=100; 
-epshiter=0; 
-epsQeval=0.1; 
-discount = 0.6;
-iter=0;
-tic
-[Q_optim1, h_optim1] = iteratiaQ(discount,epsQiter,epsQeval, model);
-toc
-[Q_optim2, h_optim2] = legeaDeControl(discount, epshiter, epsQeval, epsQiter, model);
-xplus = start_loc;
 
+%% Move as said by IteratiaQ
+start_loc=gridnav_problem('reset', model,'rand');
+viscfg.x = start_loc;
+xplus = start_loc;
 Q=Q_optim1;
 H=h_optim1;
+iter=0;
+terminal = 0;
 while iter<epsQiter && ~terminal
     movement=H(xplus(1),xplus(2));
     [xplus, rplus, terminal] = gridnav_mdp(model, xplus, movement);     
@@ -88,11 +71,54 @@ while iter<epsQiter && ~terminal
     viscfg.gview = gridnav_visualize(viscfg);
     iter=iter+1;
 end
-pause
 viscfg.Q = Q ;
 viscfg.x = [];
 viscfg.h = H;
 viscfg.gview =  gridnav_visualize(viscfg);
 pause
 figure
-mesh(max(Q,[],3))
+mesh(max(permute(Q,[2,1,3]),[],3))
+
+%% Move as said by legeDeControl (first epsilon)
+start_loc=gridnav_problem('reset', model,'rand');
+viscfg.x = start_loc;
+xplus = start_loc;
+Q=Q_optim2;
+H=h_optim2;
+iter=0;
+while iter<epsQiter && ~terminal
+    movement=H(xplus(1),xplus(2));
+    [xplus, rplus, terminal] = gridnav_mdp(model, xplus, movement);     
+    viscfg.x = xplus;
+    viscfg.gview = gridnav_visualize(viscfg);
+    iter=iter+1;
+end
+viscfg.Q = Q ;
+viscfg.x = [];
+viscfg.h = H;
+viscfg.gview =  gridnav_visualize(viscfg);
+pause
+figure
+mesh(max(permute(Q,[2,1,3]),[],3))
+
+%% Partea 3 - gamma comparation
+model = gridnav_problem('model', cfg);
+
+epsQiter=100; 
+epshiter=0; 
+epsQeval=1; 
+
+for gamma=0.6:0.1:0.99
+% for IteratieQ
+%     tic
+%     [Q_optim3, h_optim3, iteratii3] = iteratiaQ(gamma,epsQiter,epsQeval, model);
+%     iteratii3
+%     toc
+% for LegeaDeControl
+    tic
+    [Q_optim33, h_optim33, iteratiiMajore33, iteratiiMinore33] = legeaDeControl(gamma, epshiter, epsQeval, epsQiter, model);
+    iteratiiMajore33
+    iteratiiMinore33
+    toc
+end
+%plot(1:6,[5,2,3,3,2,2],1:6,[7,2,3,3,2,2],1:6,[10,2,3,3,2,2],1:6,[19,2,3,3,2,2]); legend('\gamma=0.6','\gamma=0.7','\gamma=0.8','\gamma=0.9')
